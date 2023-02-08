af249b9a922cc4084086d1dca8017e98ed1f4cda
Sabah Uddin <100025755+FastSabah@users.noreply.github.com>
2023-02-08 20:54:58 +0500

    Lecture 16

---
+++ src/components/User.vue
@@ -0,0 +1,41 @@
+<template>
+
+
+        <div class="user">
+    {{ data.name }}
+    <button v-on:click="getData(data.name)">Check Name</button>
+</div>
+  
+</template>
+
+    
+<script>
+
+
+export default {
+    name: "UserF",
+    props:{
+        data:Object,
+        getData:Function
+    }
+
+}
+</script>
+
+<style scoped>
+ul {
+    
+    text-align: left;
+    margin-left: 20px;
+    list-style: none;
+}
+.user{
+    background-color: rgb(0, 64, 255);
+    padding:10px;
+    margin-bottom:10px;
+    color:#fafafa;
+    text-transform: uppercase;
+
+}
+</style>
+
